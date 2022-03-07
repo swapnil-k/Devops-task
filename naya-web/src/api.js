ffbff2be2fe5db297abb87e32f62546b0d9f72f3
madhumita-chatne <madhumitachatne@gmail.com>
2022-03-07 23:38:48 +0530

    intiial commit

---
+++ naya-web/src/api.js
@@ -0,0 +1,75 @@
+export const signInAPICall = (data) => {
+  return fetch('https://multiplayer-test-backend-3towjekr2q-ue.a.run.app/api/login', {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(res => {
+      return res.json();
+    })
+    .catch(err => console.log(err))
+}
+
+export const exploreAPICall = () => {
+  return fetch('https://multiplayer-test-backend-3towjekr2q-ue.a.run.app/api/allSketches', {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      'Access-Control-Allow-Origin': 'https://multiplayer-test-backend-3towjekr2q-ue.a.run.app/'
+    }
+  }).then(response => {
+    return response.json();
+  })
+    .catch(err => console.log(err))
+}
+
+export const metadataAPICall = async (data) => {
+  return fetch('https://multiplayer-test-backend-3towjekr2q-ue.a.run.app/api/metaData', {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err))
+}
+
+export const updateSketchAPICall = async (data) => {
+  return await fetch('https://multiplayer-test-backend-3towjekr2q-ue.a.run.app/api/updateSketch', {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => console.log(err))
+}
+
+export const editAPICall = (data) => {
+  return fetch('https://multiplayer-test-backend-3towjekr2q-ue.a.run.app/api/sketchWithUserInfo', {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      console.log(response.body)
+      return response.json();
+    })
+    .catch(err => console.log(err))
+}
+
